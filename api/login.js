a0619da1be8c558f254bf379e07b6ec6785615df
Pedro Martín Gómez <pmargom@gmail.com>
2016-06-24 14:39:39 +0100

    Add files via upload

---
+++ api/login.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    utils = require('./utils');
+
+module.exports = function() {
+    var router = express.Router();
+     
+    router.post('/', function(req, res, next) {
+        var query = {
+            sql: 'SELECT count(*) as userExist from users WHERE email=@email AND password=@password', 
+            parameters: [
+                { name: 'email', value: req.body.email }, 
+                { name: 'password', value: utils.md5(req.body.password) }      
+            ]
+        };
+        
+        req.azureMobile.data.execute(query)
+        .then(function (results) {
+            res.json({ data: results[0] });
+        })
+        .catch(function (err) {
+           res.json(400, err);
+        });
+    });
+    
+    return router;
+};
