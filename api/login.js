9a1e188ea3890bbc2adaf302a225d246c4da2996
Pedro Martín <pmargom@gmail.com>
2016-06-26 22:37:01 +0100

    no message

--- api/login.js
+++ api/login.js
@@ -23,13 +23,13 @@ module.exports = function() {
         });
     });
 
-    router.get('/fb', function(req, res, next) {
-        req.azureMobile.user.getIdentity("facebook").then((data) => {
-            res.status(200).type('application/json').json(data);
-        }).catch((error) => {
-            res.status(500).send(JSON.stringify(error));
-        });
-    });
+    //router.get('/fb', function(req, res, next) {
+    //    req.azureMobile.user.getIdentity("facebook").then((data) => {
+    //        res.status(200).type('application/json').json(data);
+    //    }).catch((error) => {
+    //        res.status(500).send(JSON.stringify(error));
+    //    });
+    //});
     
     return router;
 };
