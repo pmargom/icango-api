c71bfedbdf6119f5afcd13c678ec721bea974ed6
Pedro Martín <pmargom@gmail.com>
2016-07-09 15:47:30 +0100

    Services api - added put action to modify a specified service id

--- api/services.js
+++ api/services.js
@@ -160,6 +160,50 @@ module.exports = function() {
          });
    });
 
+   // Modify service
+   router.put('/:id', function(req, res, next) {
+      var db = req.azureMobile.data;
+      var query = {
+         sql: 'CreateService @id, @name,@description,@price,@tags,@idUserRequest,@latitude,@longitude,@status',
+         parameters: [
+            { name: 'id', value: req.params.id },
+            { name: 'name', value: req.body.name },
+            { name: 'description', value: req.body.description },
+            { name: 'price', value: req.body.price },
+            { name: 'tags', value: req.body.tags },
+            { name: 'idUserRequest', value: req.body.idUserRequest },
+            { name: 'latitude', value: req.body.latitude },
+            { name: 'longitude', value: req.body.longitude },
+            { name: 'status', value: req.body.status }
+         ]
+      };
+
+      db.execute(query)
+         .then(function (results) {
+            if (results.length > 0)
+               res.json({
+                  totalRows: results.length,
+                  error: '',
+                  data: results
+               });
+            else
+               res.json({
+                  totalRows: 0,
+                  error: 'No data found',
+                  data: {}
+               });
+
+         })
+         .catch(function (err) {
+            //res.json(400, err);
+            res.json({
+               totalRows: 0,
+               error: err,
+               data: {}
+            });
+         });
+   });
+
    return router;
 };
 
